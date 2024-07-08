0e701d20da6fea8da90d9978d4ad7d3a6692a615
Ahmed Fouad <dev@depkey.com>
2024-07-08 06:22:34 +0300

    Iniate

---
+++ AhmedSignalR/Hubs/UserHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AhmedSignalR.Hubs
+{
+    public class UserHub: Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+        public UserHub() { }
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
+        }
+    }
+}
---
+++ AhmedSignalR/wwwroot/js/UserCount.js
@@ -0,0 +1,27 @@
+﻿// 1- create connection
+var connectionUserCount = new signalR.HubConnectionBuilder()
+    //.configureLogging(signalR.LogLevel.Information)
+    //.withAutomaticReconnect()
+    .withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets).build();
+
+//connect to methods that hub invokes aka receive notfications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    debugger;
+    $("#totalViewsCounter").text(value.toString());
+});
+ 
+// 2- invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient()
+{
+    connectionUserCount.invoke("NewWindowLoaded");
+}
+
+function fulfield() {
+    console.log("SignlR Connected");
+    newWindowLoadedOnClient();
+}
+function rejected() {
+    console.log("rejected");
+}
+// 3- Start Connection
+connectionUserCount.start().then(fulfield, rejected);
