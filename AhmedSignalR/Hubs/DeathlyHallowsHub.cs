f4d3c09ec5c69eb5c33bb1e32105f57d929a7671
Ahmed Fouad <dev@depkey.com>
2024-07-09 07:14:50 +0300

    SignalR Ex2

---
+++ AhmedSignalR/Hubs/DeathlyHallowsHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRSample;
+
+namespace AhmedSignalR.Hubs
+{
+    public class DeathlyHallowsHub : Hub
+    {
+        public Dictionary<string, int> GetRaceStatus()
+        {
+            return SD.DealthyHallowRace;
+        }
+    }
+}
--- AhmedSignalR/Hubs/UserHub.cs
+++ AhmedSignalR/Hubs/UserHub.cs
@@ -5,12 +5,28 @@ namespace AhmedSignalR.Hubs
     public class UserHub: Hub
     {
         public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
         public UserHub() { }
 
-        public async Task NewWindowLoaded()
+        public async Task<string> NewWindowLoaded()
         {
             TotalViews++;
-            await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+            return $"Total Views: {TotalViews}";
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            TotalUsers++;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers--;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
         }
     }
 }
